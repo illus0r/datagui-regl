08b97ca9f6afec1ef21160a8a8e9fbbd30cfc0c4
illus0r <zor667@gmail.com>
2020-12-12 03:28:02 +0400

    init

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"node_modules/dat.gui/build/dat.gui.module.js":[function(require,module,exports) {
+})({"../node_modules/dat.gui/build/dat.gui.module.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -3025,7 +3025,7 @@ var index = {
 };
 var _default = index;
 exports.default = _default;
-},{}],"node_modules/regl/dist/regl.js":[function(require,module,exports) {
+},{}],"../node_modules/regl/dist/regl.js":[function(require,module,exports) {
 var define;
 var global = arguments[3];
 (function (global, factory) {
@@ -13560,7 +13560,7 @@ regl.frame(function () {
     regl.draw();
   });
 });
-},{"dat.gui":"node_modules/dat.gui/build/dat.gui.module.js","regl":"node_modules/regl/dist/regl.js"}],"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"dat.gui":"../node_modules/dat.gui/build/dat.gui.module.js","regl":"../node_modules/regl/dist/regl.js"}],"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -13588,7 +13588,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62612" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64056" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
