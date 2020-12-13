5877ee98c8393aa6c83159ab8bd2c8924133b5dd
illus0r <zor667@gmail.com>
2020-12-13 23:58:36 +0400

    glslifalization

---
+++ src/frag.glsl
@@ -0,0 +1,8 @@
+precision mediump float;
+varying vec2 uv;
+uniform float red;
+uniform float green;
+uniform float blue;
+void main() {
+	gl_FragColor = vec4(red, green, blue, 1.);
+}`,
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// CommonJS:
+let fs = require('fs');
+const glslify = require('glslify')
 const dat = require('dat.gui');
 const gui = new dat.GUI();
 
-var params = {red: 1, green: 0, blue: 0};
+let params = {red: 1, green: 0, blue: 0};
 gui.add(params, 'red', 0, 1);
 gui.add(params, 'green', 0, 1);
 gui.add(params, 'blue', 0, 1);
@@ -11,39 +12,23 @@ const regl = require('regl')(document.body)
 
 
 const setupQuad = regl({
-  frag: `
-  precision mediump float;
-  varying vec2 uv;
-	uniform float red;
-	uniform float green;
-	uniform float blue;
-  void main() {
-    gl_FragColor = vec4(red, green, blue, 1.);
-  }`,
+  vert: glslify('./vert.glsl'),
+  frag: glslify('./frag.glsl'),
 
-  vert: `
-  precision mediump float;
-  attribute vec2 position;
-  varying vec2 uv;
-  void main() {
-    uv = 0.5 * (position + 1.0);
-    gl_Position = vec4(position, 0, 1);
-  }`,
-
-  attributes: {
-    position: [ -4, -4, 4, -4, 0, 4 ]
-  },
+	attributes: {
+		position: [ -4, -4, 4, -4, 0, 4 ]
+	},
 
 	uniforms: {
-    tick: regl.context('tick'),
+		tick: regl.context('tick'),
 		red: () => {return params.red},
 		green: () => {return params.green},
 		blue: () => {return params.blue},
-  },
+	},
 
-  depth: { enable: false },
+	depth: { enable: false },
 
-  count: 3
+	count: 3
 })
 
 regl.frame(() => {
